79e9d933428ba4216e7bcce5cae0c00090d1785c
Ben Lackey <ben.lackey@outlook.com>
2018-10-12 15:45:46 -0700

    no lb for single ad

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -14,20 +14,6 @@ resource "oci_containerengine_cluster" "cluster" {
   kubernetes_version = "${var.oke["version"]}"
   name               = "${var.oke["name"]}"
   vcn_id             = "${oci_core_virtual_network.virtual_network.id}"
-
-  options {
-    service_lb_subnet_ids = ["${oci_core_subnet.subnet.id}"]
-
-    add_ons {
-      is_kubernetes_dashboard_enabled = true
-      is_tiller_enabled               = true
-    }
-
-    kubernetes_network_config {
-      pods_cidr     = "10.244.0.0/16"
-      services_cidr = "10.96.0.0/16"
-    }
-  }
 }
 
 resource "oci_containerengine_node_pool" "node_pool" {
